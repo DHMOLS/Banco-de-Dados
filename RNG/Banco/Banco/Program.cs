6a3b9e5d8679047c18da552a8104b1a504abb712
Rafaela Gonçalves <rafaelaga46@gmail.com>
2018-02-01 18:04:56 -0200

    rng

---
+++ RNG/Banco/Banco/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rng = rnd.Next(0, 9);
+            int rng2 = rnd.Next(0, 9);
+            int rng3 = rnd.Next(0, 9);
+            int rng4 = rnd.Next(0, 9);
+
+            Console.Write(rng);
+            Console.Write(rng2);
+            Console.Write(rng3);
+            Console.Write(rng4);
+
+            Console.ReadLine();
+        }
+    }
+}
